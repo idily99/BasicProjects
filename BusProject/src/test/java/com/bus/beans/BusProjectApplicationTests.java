145e69b7d054f486444782ce8ce843d16bcf3ac1
idily99 <vadasamber99@gmail.com>
2023-05-20 11:57:49 +0530

    change the feature300 and add url

--- BusProject/src/test/java/com/bus/beans/BusProjectApplicationTests.java
+++ BusProject/src/test/java/com/bus/beans/BusProjectApplicationTests.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BusProjectApplicationTests {
-
+class BusProjectApplicationTests 
+{
+private Static final String url="https://github.com/idily99/BasicProjects.git";
 	@Test
 	void contextLoads()
  {
 Map<String,Object> map=new HashMap<String,Object>();
 System.out.println("fine");
-	}
+}
 
 }
